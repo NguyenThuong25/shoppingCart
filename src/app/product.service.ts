09f879a05d5f72d510607c3bc9a8c2ec628030ac
Nguyễn Thị Thương (VINID-KCN-MB) <thuongnt26@vingroup.net>
2019-11-22 15:42:11 +0700

    fix Failing To Load Images From Assets:./assets/image.jpn

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,7 +9,7 @@ export class ProductService {
       id: 0,
       name: "GIÀY BẢO HỘ LAO ĐỘNG",
       description: "giày được làm bằng vải giúp bảo vệ chân",
-      image: "/assets/sp1.jpg",
+      image: "./assets/sp1.jpg",
       price: 15000,
       tax: 1000,
       quantity: 1
@@ -18,7 +18,7 @@ export class ProductService {
       id: 1,
       name: "GIÀY THỜI TRANG NAM",
       description: "giày nam đẹp giá rẻ",
-      image: "/assets/sp2.jpg",
+      image: "./assets/sp2.jpg",
       price: 90000,
       tax: 2000,
       quantity: 1
